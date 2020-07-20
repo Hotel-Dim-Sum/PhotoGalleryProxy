d3a8ce474d31445699faf077276bc119872f33b7
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-20 10:42:27 -0700

    Updates Proxy to Work with Postgres Server
    - Updates middleware to hit new CRUD routes
    - Creates node cluster index.js file for stress testing

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// const newRelic = require('newrelic');
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -14,6 +15,7 @@ app.use(cors());
 // app.use('/api', createProxyMiddleware({ target: 'http://52.14.226.255/', changeOrigin: true }));
 // app.use('/reviews', createProxyMiddleware({ target: 'http://18.224.95.187/', changeOrigin: true }));
 // app.use('/carousel', createProxyMiddleware({ target: 'http://54.215.84.53/', changeOrigin: true }));
-app.use('/photogallery', createProxyMiddleware({ target: 'http://localhost:3004/', changeOrigin: true }));
+app.use('/api/:roomId/photo-gallery', createProxyMiddleware({ target: 'http://localhost:3004/', changeOrigin: true }));
+app.use('/api/:roomId/photo-gallery/save-status', createProxyMiddleware({ target: 'http://localhost:3004/', changeOrigin: true }));
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}/`));
